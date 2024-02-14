c1169668e5154eb648db8bc0f5b4ed377c0a86bc
Yashwanth <ananthayashwanth143@gmail.com>
2024-02-14 17:44:05 +0530

    Added Navbar component

--- PensionManagementUI/src/app/app.module.ts
+++ PensionManagementUI/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './features/auth/sign-up/sign-up.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent,  
+    SignUpComponent,
+    NavbarComponent,  
   ],
   imports: [
     BrowserModule,
