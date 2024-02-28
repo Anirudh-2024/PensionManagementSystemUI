898032ea4080239aeb42dcdeb336c52252d44b33
Yashwanth <ananthayashwanth143@gmail.com>
2024-02-28 16:16:30 +0530

    Pensioner & Guardian Api Calls

---
+++ PensionManagementUI/src/app/features/pages/pensioner/models/pensionerplan.model.ts
@@ -0,0 +1,10 @@
+export interface PensionerPlan{
+    PensionerPlanId: string;
+    PensionPlanName: string;
+    Amount: number;
+    StartDate: Date;
+    EndDate: Date;
+    PensionDetails: string;
+
+    
+}
---
+++ PensionManagementUI/src/app/features/pages/pensioner/models/pensionrequest.model.ts
@@ -0,0 +1,11 @@
+export interface PensionRequest{
+    FullName: string;
+    DateOfBirth: Date;
+    Gender: string;
+    AadharNumber: string;
+    PhoneNumber: string;
+    Address: string;
+    Age: number;
+    Id: string;
+    PensionPlanId: string
+}
