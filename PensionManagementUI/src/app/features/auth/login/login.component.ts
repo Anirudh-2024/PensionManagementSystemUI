87a7395c1b8ebc0889f82d423f0debf4791d9c07
Sivaadda <sivapriyaadda@gmail.com>
2024-03-11 16:11:21 +0530

    Added routing and changed url

--- PensionManagementUI/src/app/features/auth/login/login.component.ts
+++ PensionManagementUI/src/app/features/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { login } from './models/login.model';
 import { LoginService } from './services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent {
   model:login;
   token:''
 
-  constructor(private loginService:LoginService){
+  constructor(private loginService:LoginService, private router:Router){
   this.model={
     email:'',
     password:''
@@ -26,7 +27,7 @@ export class LoginComponent {
         localStorage.setItem('userId',response.id);
         localStorage.setItem('userEmail',response.email);
         localStorage.setItem('token',response['token']);
-        alert("Login successfull");
+        this.router.navigate(['/'])
       },
       error:(error)=>{
         console.log(error.message);
